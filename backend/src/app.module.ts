7b1ca161a2455cdd2387b839c8b6f1c76a1aba55
kmasuoka <masuoka@h-company.co.jp>
2025-07-09 19:55:50 +0900

    アプリ側のGraphQLの設定を追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      graphiql: true,
+      autoSchemaFile: join(process.cwd(), 'src/shared/graphql/schema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
