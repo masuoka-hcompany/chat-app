57dfba2b1a6a7ca1c6e226a81a6413133696aa73
kmasuoka <masuoka@h-company.co.jp>
2026-01-04 16:03:13 +0900

    Userのリポジトリに実装に必要なメソッドを追加

--- backend/src/modules/user/repositories/interfaces/interface.user.repository.ts
+++ backend/src/modules/user/repositories/interfaces/interface.user.repository.ts
@@ -2,6 +2,22 @@ import { User } from '../../graphql-types/objects/user.model';
 
 export interface IUserRepository {
   findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  findByAccount(
+    provider: string,
+    providerAccountId: string,
+  ): Promise<User | null>;
+  createUserWithAccount(data: {
+    email: string;
+    image?: string;
+    provider: string;
+    providerAccountId: string;
+  }): Promise<User>;
+  addAccountToUser(
+    userId: string,
+    provider: string,
+    providerAccountId: string,
+  ): Promise<User>;
 }
 
 export const IUserRepositoryToken = Symbol('IUserRepository');
--- backend/src/modules/user/repositories/user.prisma.repository.ts
+++ backend/src/modules/user/repositories/user.prisma.repository.ts
@@ -18,4 +18,71 @@ export class UserPrismaRepository implements IUserRepository {
       },
     });
   }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findFirst({
+      where: {
+        email: email,
+        userStatusId: 'ACTIVE',
+      },
+      include: {
+        profile: true,
+        userStatus: true,
+      },
+    });
+  }
+
+  async findByAccount(
+    provider: string,
+    providerAccountId: string,
+  ): Promise<User | null> {
+    return this.prisma.user.findFirst({
+      where: {
+        accounts: {
+          some: {
+            authProviderId: provider,
+            providerAccountId,
+          },
+        },
+      },
+    });
+  }
+
+  async createUserWithAccount(data: {
+    email: string;
+    image?: string;
+    provider: string;
+    providerAccountId: string;
+  }): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        userStatusId: 'ACTIVE',
+        accounts: {
+          create: {
+            authProviderId: data.provider,
+            providerAccountId: data.providerAccountId,
+          },
+        },
+      },
+    });
+  }
+
+  async addAccountToUser(
+    userId: string,
+    provider: string,
+    providerAccountId: string,
+  ): Promise<User> {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        accounts: {
+          create: {
+            authProviderId: provider,
+            providerAccountId: providerAccountId,
+          },
+        },
+      },
+    });
+  }
 }
